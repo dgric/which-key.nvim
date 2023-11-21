02d1c3b9d44a87d696639a37d9971cef00bf08c9
David Gricar <git@coppie.xyz>
2023-11-21 15:44:57 +0100

    Allow sort by description
    Applies willothy's patch
    https://github.com/willothy/which-key.nvim/commit/48d1e60

--- lua/which-key/config.lua
+++ lua/which-key/config.lua
@@ -63,6 +63,7 @@ local defaults = {
   hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " }, -- hide mapping boilerplate
   show_help = true, -- show a help message in the command line for using WhichKey
   show_keys = true, -- show the currently pressed key and its label as a message in the command line
+  sort_by_desc = false,
   triggers = "auto", -- automatically setup triggers
   -- triggers = {"<leader>"} -- or specifiy a list manually
   -- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
